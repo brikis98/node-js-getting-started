f42e5959386a8bde68876a87f0f35fea22d1e91f
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2021-07-15 17:09:42 +0100

    Try to fix SSL settings

--- index.js
+++ index.js
@@ -5,14 +5,9 @@ const PORT = process.env.PORT || 5000
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: !process.env.DATABASE_URL.includes('localhost')
 });
-
-if (!process.env.DATABASE_URL.includes('localhost')) {
-  pool.ssl = {
-    rejectUnauthorized: false
-  }
-}
 
 const showTimes = () => {
   let result = '';
